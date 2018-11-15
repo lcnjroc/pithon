0bf982aecaef78c2fcd9862633a900a253c0a9de
Jared M <lcnjroc@gmail.com>
2018-11-15 15:18:03 -0500

    Second changes and audio test.

--- controller.py
+++ controller.py
@@ -5,7 +5,7 @@ import wave
 def playaudio(filename):
 	chunk = 1024 # split our audio file into chunks
 	wf = wave.open(filename, 'rb') # open the audio file as a readable file
-	pa = pyaudio.pyAudio() # instantiate the pyAudio class
+	pa = pyaudio.PyAudio() # instantiate the pyAudio class
 
 	stream = pa.open(
 		format=pa.get_format_from_width(wf.getsampwidth()),
